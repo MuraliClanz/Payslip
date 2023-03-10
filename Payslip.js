fdf92e40b7eca0c99c8619a2c02632e948e59336
Murali S <105419559+MuraliClanz@users.noreply.github.com>
2023-03-11 00:30:17 +0530

    Add files via upload

---
+++ Payslip.js
@@ -0,0 +1,51 @@
+
+
+let payment = {
+    "_id": "640a1bcc10067ae5dc2b",
+    "user_id": "640a1a6252b6cd95f74",
+    "month": 2,
+    "year": 2023,
+    "earnings": [
+        {
+            "title": "basic pay",
+            "amount": 15000,
+            "_id": "640a1bcc10067ae5d4fcac2c"
+        },{
+            "title": "allowance pay",
+            "amount": 1000,
+            "_id": "640a1bcc10067ae5d4fcac3c"
+        },
+    ],
+    "deductions": [
+        {
+            "title": "pf",
+            "amount": 1500,
+            "_id": "640a1bcc10067ae5d4fcac2d"
+        },
+        {
+            "title": "professional tax",
+            "amount": 500,
+            "_id": "640a1bcc10067ae5d4fcac2e"
+        }
+    ],
+    "work_days": 28,
+    "lop_days": 2,
+    "createdAt": "2023-03-09T17:47:57.007Z",
+    "updatedAt": "2023-03-09T17:47:57.007Z"
+}
+
+let earnings = 0;
+payment.earnings.forEach(item=>earnings+=item.amount);
+
+console.log("earnings amount",earnings);
+
+let deductions = 0;
+
+payment.deductions.forEach(item=>deductions+=item.amount);
+
+console.log("deductions amount",deductions);
+
+
+let Totalearnings = earnings - deductions;
+console.log("Totalearnings amount",Totalearnings);
+
